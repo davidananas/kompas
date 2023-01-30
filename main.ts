29970103238b332bb5cb8b09a7ea43d02bd3a0f6
davidananas <116786922+davidananas@users.noreply.github.com>
2023-01-30 17:11:10 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 basic.forever(function () {
-    if (input.compassHeading() > 159 || input.compassHeading() < 30) {
-        basic.showIcon(IconNames.Yes)
-    } else {
-        basic.showIcon(IconNames.No)
+    if (input.magneticForce(Dimension.Y) == 0) {
+        basic.showIcon(IconNames.Happy)
     }
 })
